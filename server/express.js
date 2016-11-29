d676ae72d1815dd7f3f4a02f9b2efb82ffe532f4
Igor Vieira <igorpereiraramosvieira@gmail.com>
2016-11-29 00:20:42 -0200

    remove modules

--- server/express.js
+++ server/express.js
@@ -1,31 +1,15 @@
 import express from 'express'
 import consign from 'consign'
 import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import session from 'express-session'
 
 
 
 const app = express()
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use(cookieParser());
-	app.use(session(
-		{
-			secret:'The Machine',
-			resave:true,
-			saveUninitialized:true
-		}
-	));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(require('method-override')());
+
 
 app.set('port', (process.env.PORT || 3000))
 
